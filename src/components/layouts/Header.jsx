d4cc5c564ebe2ffc5b6a82c4986761fd237cbac9
GuyRegevv <gregev7@gmail.com>
2025-03-24 22:32:54 +0200

    starting the header

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,10 +1,17 @@
+"use client"
 import React from 'react';
 
 export const Header = () => {
+    const handleHomeClick = () => {
+        console.log('Home clicked');
+    }
     return (
         <div className="w-full h-20 flex border-4 border-red-400">
-            <div className="border-2 border-blue-300">
-                
+            <div className="w-1/3 flex justify-center items-center border-2 border-blue-300">
+                <button className="w-16 h-16 mx-3 border-2 rounded"
+                        onClick={handleHomeClick}>Home</button>
+                <button className="w-16 h-16 mx-3 border-2 rounded">Saved</button>
+                <button className="w-15 h-16 mx-3 border-2 rounded">New</button>
             </div>
         </div>
     )
