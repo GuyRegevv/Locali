c3735670f0f1ac6f1c00cf5a2f37b9e658fd901b
GuyRegevv <gregev7@gmail.com>
2025-03-24 21:42:12 +0200

    first one

---
+++ src/app/(autherized)/(home)/page.jsx
@@ -0,0 +1,10 @@
+import { Header } from "@/components/layouts/Header"
+
+export default function HomePage () {
+    return (
+        <div>
+            <Header className="w-full border-16 border-red-400"></Header>
+            <div className="border-16 border-amber-300">Hello world</div>
+        </div>
+    )
+}
---
+++ src/components/layouts/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Header = () => {
+    return (
+        <div className="w-full h-20 flex border-4 border-red-400">
+            <div className="border-2 border-blue-300">
+                
+            </div>
+        </div>
+    )
+} 
