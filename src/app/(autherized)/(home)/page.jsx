14ef6dbf2e57432108a51fd58247384b68952ed1
talmagal2 <tal@sunfltd.com>
2025-05-28 21:47:49 +0300

    added animation to the home page

--- src/app/(autherized)/(home)/page.jsx
+++ src/app/(autherized)/(home)/page.jsx
@@ -1,19 +1,7 @@
+'use client'
 import { UIButton } from "@/components/ui/UIButton";
 import { SearchForm } from "./SearchForm";
-
-// export default function HomePage () {
-//     return (
-//       <>
-//           <div className="flex flex-col w-1/2 border-4 border-pink-400 "> 
-//             <div className="border-4 border-brown-400 bg-cover">
-//                 <p className="m-5 text-7xl font-bold">Find travel recommendation from real locals</p>
-//                 <p className="m-5 text-5xl font-bold">for any kind of activity you like to do when travelling</p>
-//                 <p className="m-5 text-3xl font-bold">Food, Clothing, Museums, attractions, parks, bars and more</p>
-//             </div>
-//             {/* animation */}
-//           <div className="flex-1 border-4 border-blue-400 ">animation</div>
-//           </div>
-// =======
+import TravelAnimation from "@/components/animations/TravelAnimation";
 
 export default function HomePage() {
   return (
@@ -30,8 +18,9 @@ export default function HomePage() {
             Food, Clothing, Museums, attractions, parks, bars and more
           </p>
         </div>
-        {/* animation */}
-        <div className="flex-1 border-4 border-blue-400">animation</div>
+        
+        {/* Simple Travel Animation */}
+        <TravelAnimation />
       </div>
         
       <div className="flex items-center justify-center w-1/2 border-4 border-yellow-400">
